57f8f89bf545aefefdf4eecfb8f7fc24b9b23a16
Nogueira Terra Mandolesi <ginogue@cmtclm504mkc4x2.rd.corpintra.net>
2023-09-18 18:06:15 +0200

    Updated Fortnite Calendar

--- supporting_scripts/Fortnite.py
+++ supporting_scripts/Fortnite.py
@@ -2,9 +2,39 @@ from ics import Calendar, Event
 c = Calendar()
 
 e = Event()
-e.name = "Battle Royale Mix-Up Monday"
-e.begin = '2023-09-18 15:00:00'
-e.end = '2023-09-19 03:00:00'
+e.name = "Battle Royale Mix-Up Monday - Europe"
+e.begin = '2023-09-18 17:00:00'
+e.end = '2023-09-19 19:00:00'
+c.events.add(e)
+
+e = Event()
+e.name = "Battle Royale Mix-Up Monday - Europe"
+e.begin = '2023-09-25 17:00:00'
+e.end = '2023-09-26 19:00:00'
+c.events.add(e)
+
+e = Event()
+e.name = "Battle Royale Mix-Up Monday - Europe"
+e.begin = '2023-10-02 17:00:00'
+e.end = '2023-10-02 19:00:00'
+c.events.add(e)
+
+e = Event()
+e.name = "Battle Royale Mix-Up Monday - Europe"
+e.begin = '2023-10-16 17:00:00'
+e.end = '2023-10-16 19:00:00'
+c.events.add(e)
+
+e = Event()
+e.name = "Battle Royale Mix-Up Monday - Europe"
+e.begin = '2023-10-23 17:00:00'
+e.end = '2023-10-23 19:00:00'
+c.events.add(e)
+
+e = Event()
+e.name = "Battle Royale Mix-Up Monday - Europe"
+e.begin = '2023-10-30 16:00:00'
+e.end = '2023-10-30 18:00:00'
 c.events.add(e)
 
 e = Event()
@@ -41,18 +71,6 @@ e = Event()
 e.name = "Victory Cup"
 e.begin = '2023-09-24 05:00:00'
 e.end = '2023-09-25 02:00:00'
-c.events.add(e)
-
-e = Event()
-e.name = "Battle Royale Mix-Up Monday"
-e.begin = '2023-09-25 08:00:00'
-e.end = '2023-09-25 10:00:00'
-c.events.add(e)
-
-e = Event()
-e.name = "Battle Royale Mix-Up Monday"
-e.begin = '2023-09-25 15:00:00'
-e.end = '2023-09-26 02:00:00'
 c.events.add(e)
 
 e = Event()
